54e3a8832212fc21d6d32e14576cae61264d011d
Kirill Bushmanov <53367565+dissin101@users.noreply.github.com>
2024-12-21 16:24:23 +0500

    Add base dialog window

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,49 @@
 import { io } from 'socket.io-client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { getCurrentDateTime } from './utils/time.ts';
 const apiPort: string = import.meta.env.VITE_SERVER_PORT;
 
 const App = () => {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [input, setInput] = useState<string>('');
   const socket = io(`http://localhost:${apiPort}`);
 
   useEffect(() => {
     socket.on('connect', () => {
-      console.log('connected');
-
-      socket.timeout(5000).emit('chat message', 'test');
+      console.log('Connected to server');
     });
+
+    socket.on('chat message', (message: string) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
   }, []);
 
-  return <>Client app</>;
+  const sendMessage = () => {
+    if (input.trim()) {
+      socket.emit('chat message', `${input} - ${getCurrentDateTime()}`);
+      setInput('');
+    }
+  };
+
+  return (
+    <div>
+      <div style={{ display: 'flex', gap: 2 }}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Type a message"
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+      <p>Чат:</p>
+      {messages?.map((message, index) => <p key={index}>{message}</p>)}
+    </div>
+  );
 };
 
 export default App;
